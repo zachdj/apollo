52177732f64587d69d0e468116af33f55293e0d7
Zach Jones <zachydj@gmail.com>
2019-03-23 22:34:03 -0400

    Refactor experiments to work with new validation API

--- experiments/figures.py
+++ experiments/figures.py
@@ -23,11 +23,14 @@ def _irr_vs_hour(start, stop, output):
     unix_stop = stop.value / 10 ** 9
 
     # we convert from utc to est hour
-    query = f'SELECT (HOUR + 20)%24 + 1 as hour, ' \
-            f'AVG(UGAAPOA1IRR) as Array_A, AVG(UGABPOA1IRR) as Array_B, AVG(UGAEPOA1IRR) as Array_E '
-    query += f' FROM IRRADIANCE WHERE timestamp BETWEEN {unix_start} AND {unix_stop}' \
-             f' GROUP BY HOUR' \
-             f' ORDER BY HOUR;'
+    query = f' SELECT (HOUR + 20)%24 + 1 as hour,' \
+            f' AVG(UGAAPOA1IRR) as Array_A,' \
+            f' AVG(UGABPOA1IRR) as Array_B,' \
+            f' AVG(UGAEPOA1IRR) as Array_E' \
+            f' FROM IRRADIANCE WHERE timestamp' \
+            f' BETWEEN {unix_start} AND {unix_stop}' \
+            f' GROUP BY HOUR' \
+            f' ORDER BY HOUR;'
 
     # lod data into df
     df = pd.read_sql_query(sql=query, con=connection, index_col='hour')
@@ -36,9 +39,12 @@ def _irr_vs_hour(start, stop, output):
     fig, axes = plt.subplots()
     opacity = 0.6
     axes.set_title('Mean Irradiance vs Time-of-Day')
-    line1, = axes.plot(df.index, df['Array_A'], color='r', alpha=opacity, label='Array A')
-    line2, = axes.plot(df.index, df['Array_B'], color='g', alpha=opacity, label='Array B')
-    line3, = axes.plot(df.index, df['Array_E'], color='b', alpha=opacity, label='Array E')
+    line1, = axes.plot(df.index, df['Array_A'],
+                       color='r', alpha=opacity, label='Array A')
+    line2, = axes.plot(df.index, df['Array_B'],
+                       color='g', alpha=opacity, label='Array B')
+    line3, = axes.plot(df.index, df['Array_E'],
+                       color='b', alpha=opacity, label='Array E')
     axes.set_xticks(range(1, 25))
     axes.set_xlabel('Hour (EST)')
     axes.set_ylabel('Mean Irradiance (watts / m$^2$)')
@@ -62,11 +68,14 @@ def _irr_vs_month(start, stop, output):
     unix_stop = stop.value / 10 ** 9
 
     # we convert from utc to est hour
-    query = f'SELECT MONTH as month, ' \
-            f'AVG(UGAAPOA1IRR) as Array_A, AVG(UGABPOA1IRR) as Array_B, AVG(UGAEPOA1IRR) as Array_E '
-    query += f' FROM IRRADIANCE WHERE timestamp BETWEEN {unix_start} AND {unix_stop}' \
-             f' GROUP BY month' \
-             f' ORDER BY month;'
+    query = f' SELECT MONTH as month, ' \
+            f' AVG(UGAAPOA1IRR) as Array_A,' \
+            f' AVG(UGABPOA1IRR) as Array_B,' \
+            f' AVG(UGAEPOA1IRR) as Array_E' \
+            f' FROM IRRADIANCE WHERE timestamp' \
+            f' BETWEEN {unix_start} AND {unix_stop}' \
+            f' GROUP BY month' \
+            f' ORDER BY month;'
 
     # lod data into df
     df = pd.read_sql_query(sql=query, con=connection, index_col='month')
@@ -77,12 +86,16 @@ def _irr_vs_month(start, stop, output):
 
     fig, axes = plt.subplots()
     axes.set_title('Mean Irradiance per Month')
-    rects1 = axes.bar(df.index - bar_width, df['Array_A'], width=bar_width, color='r', alpha=opacity, label='Array A')
-    rects2 = axes.bar(df.index, df['Array_B'], width=bar_width, color='g', alpha=opacity, label='Array B')
-    rects3 = axes.bar(df.index + bar_width, df['Array_E'], width=bar_width, color='b', alpha=opacity, label='Array E')
+    rects1 = axes.bar(df.index - bar_width, df['Array_A'], width=bar_width,
+                      color='r', alpha=opacity, label='Array A')
+    rects2 = axes.bar(df.index, df['Array_B'], width=bar_width,
+                      color='g', alpha=opacity, label='Array B')
+    rects3 = axes.bar(df.index + bar_width, df['Array_E'], width=bar_width,
+                      color='b', alpha=opacity, label='Array E')
     axes.set_xticks(range(1, 13))
     axes.set_xticklabels((
-        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
     ))
     axes.set_xlabel('Month')
     axes.set_ylabel('Mean Irradiance (watts / m$^2$)')
@@ -105,31 +118,41 @@ def _irr_correlations(start, stop, output):
     unix_start = start.value // 10 ** 9
     unix_stop = stop.value / 10 ** 9
 
-    targets_query = f'SELECT TIMESTAMP as reftime, ' \
-                    f'AVG(UGAAPOA1IRR) as Array_A, AVG(UGABPOA1IRR) as Array_B, AVG(UGAEPOA1IRR) as Array_E ' \
-                    f'FROM IRRADIANCE WHERE reftime BETWEEN {unix_start} AND {unix_stop} ' \
-                    f'AND hour=18 ' \
-                    f'GROUP BY hour, day, month, year;'
+    targets_query = f' SELECT TIMESTAMP as reftime, ' \
+                    f' AVG(UGAAPOA1IRR) as Array_A,' \
+                    f' AVG(UGABPOA1IRR) as Array_B,' \
+                    f' AVG(UGAEPOA1IRR) as Array_E ' \
+                    f' FROM IRRADIANCE WHERE reftime' \
+                    f' BETWEEN {unix_start} AND {unix_stop} ' \
+                    f' AND hour=18 ' \
+                    f' GROUP BY hour, day, month, year;'
     # load data and aggregate by hour
-    targets_ds = pd.read_sql_query(sql=targets_query, con=connection, index_col='reftime', parse_dates=['reftime'])
+    targets_ds = pd.read_sql_query(sql=targets_query, con=connection,
+                                   index_col='reftime', parse_dates=['reftime'])
     targets_ds = targets_ds.dropna()
-    targets_ds.index = pd.to_datetime(targets_ds.index, unit='s')  # timestamp index is unix epoch (in seconds)
+    # timestamp index is unix epoch (in seconds)
+    targets_ds.index = pd.to_datetime(targets_ds.index, unit='s')
     targets_ds.index.name = 'reftime'
     targets_ds = targets_ds.to_xarray()
 
     cloud_cover_var = 'TCC_EATM'
     air_temp_var = 'TMP_SFC'
     downward_flux_var = 'DLWRF_SFC'
-    cloud_cover_ds = SolarDataset(start=start, stop=stop, target=None, forecast=0,
-                                  feature_subset=(cloud_cover_var, air_temp_var, downward_flux_var),
-                                  temporal_features=False, geo_shape=(1,1), standardize=False, target_hours=0).xrds
+    cloud_cover_ds = SolarDataset(
+        start=start, stop=stop, target=None, forecast=0,
+        feature_subset=(cloud_cover_var, air_temp_var, downward_flux_var),
+        temporal_features=False, geo_shape=(1,1), standardize=False,
+        target_hours=0).xrds
 
-    # we have only selected data along the reftime coordinate, so the others can be dropped
-    cloud_cover_ds = cloud_cover_ds.drop(labels=('x', 'y', 'lat', 'lon', 'forecast', 'z_EATM')).squeeze()
+    # we have only selected data along the reftime coordinate
+    cloud_cover_ds = cloud_cover_ds.drop(
+        labels=('x', 'y', 'lat', 'lon', 'forecast', 'z_EATM')).squeeze()
 
     dataset = xr.merge([targets_ds, cloud_cover_ds], join='inner')
 
-    array_a, array_b, array_e = dataset['Array_A'].values, dataset['Array_B'].values, dataset['Array_E'].values
+    array_a = dataset['Array_A'].values
+    array_b = dataset['Array_B'].values
+    array_e = dataset['Array_E'].values
 
     # plot irradiance vs cloud cover
     fig, axes = plt.subplots()
@@ -179,13 +202,13 @@ def _irr_correlations(start, stop, output):
     plt.close(fig)
 
 
-def run(output='./output/figures', start='2017-01-01', stop='2018-12-31'):
+def run(output='./output/figures', first='2017-01-01', last='2018-12-31'):
     outpath = pathlib.Path(output).resolve()
     outpath.mkdir(parents=True, exist_ok=True)
 
-    _irr_vs_hour(start=start, stop=stop, output=outpath)
-    _irr_vs_month(start=start, stop=stop, output=outpath)
-    _irr_correlations(start=start, stop=stop, output=outpath)
+    _irr_vs_hour(start=first, stop=last, output=outpath)
+    _irr_vs_month(start=first, stop=last, output=outpath)
+    _irr_correlations(start=first, stop=last, output=outpath)
 
 
 if __name__ == '__main__':
--- experiments/grid_expansion.py
+++ experiments/grid_expansion.py
@@ -1,46 +1,84 @@
 # compare the performance of models that use a 1x1, 3x3, and 5x5 grid
 
-import pandas as pd
+from math import floor
+import numpy as np
 import pathlib
+import pandas as pd
+from sklearn.metrics import mean_absolute_error as mae, \
+    mean_squared_error as mse, r2_score as r2
+from sklearn.model_selection import TimeSeriesSplit, PredefinedSplit
 
-import experiments.metrics as apollo_metrics
 from apollo.models.trees import RandomForest, GradientBoostedTrees
-from apollo.validation import cross_validate, split_validate
 
 
-def run(start='2017-01-01', stop='2018-12-31',
-        metrics=('MAE', 'MSE', 'RMSE', 'R2'),
-        method='cv', folds=5, split=0.5, output='./results'):
+def rmse(y_true, y_pred, **kwargs):
+    return mse(y_true, y_pred, **kwargs)**0.5
+
+
+_default_metrics = (mae, mse, rmse, r2)
+_default_target = 'UGABPOA1IRR'
+
+
+def run(first='2017-01-01', last='2018-12-31',
+        metrics=_default_metrics, method='cv',
+        folds=5, split_size=0.5, output='./results'):
 
     print('Grid-Size Experiment')
-    metrics = [apollo_metrics.get(metric) for metric in metrics]
     output_dir = pathlib.Path(output).resolve()
+    target_hours = np.arange(1, 25)
 
-    start, stop = pd.Timestamp(start), pd.Timestamp(stop)
+    first = pd.Timestamp(first).floor(freq='6h')
+    last = pd.Timestamp(last).floor(freq='6h')
 
     for shape in [(1, 1), (3, 3), (5, 5)]:
         shape_string = f'{shape[0]}x{shape[1]}'
-        rf = RandomForest(name=f'rf-{shape_string}', geo_shape=shape)
-        gbt = GradientBoostedTrees(name=f'gbt-{shape_string}', geo_shape=shape)
+        rf = RandomForest(
+            name=f'rf-{shape_string}', geo_shape=shape,
+            target=_default_target, target_hours=target_hours)
+        gbt = GradientBoostedTrees(
+            name=f'gbt-{shape_string}', geo_shape=shape,
+            target=_default_target, target_hours=target_hours)
 
         if method == 'cv':
-            rf_results = cross_validate(rf, first=start, last=stop,
-                                        metrics=metrics, k=folds)
-            gbt_results = cross_validate(gbt, first=start, last=stop,
-                                         metrics=metrics, k=folds)
-            outpath = pathlib.Path(
-                output_dir / 'cross_val').resolve()
+            splitter = TimeSeriesSplit(n_splits=folds)
+            outpath = pathlib.Path(output_dir / 'cross_val')
         else:
-            rf_results = split_validate(rf, first=start, last=stop,
-                                        metrics=metrics, test_size=split)
-            gbt_results = split_validate(gbt, first=start, last=stop,
-                                         metrics=metrics, test_size=split)
-            outpath = pathlib.Path(
-                output_dir / 'split_val').resolve()
+            # create custom splitter with PredefinedSplit
+            test_pct = max(0, min(split_size, 1))
+            first = pd.Timestamp(first).floor(freq='6h')
+            last = pd.Timestamp(last).floor(freq='6h')
+            # total reftimes in the selected dataset
+            reftime_count = (last - first) // pd.Timedelta(6, 'h')
+
+            # create index for PredefinedSplit
+            testing_count = floor(reftime_count * test_pct)
+            training_count = reftime_count - testing_count
+            test_fold = np.concatenate((
+                np.ones(training_count) * -1,  # -1 indicates training set
+                np.zeros(testing_count)  # 0 indicates testing set, 1st fold
+            ))
+            splitter = PredefinedSplit(test_fold)
+            outpath = pathlib.Path(output_dir / 'split')
+
+        rf_results = rf.validate(first=first, last=last,
+                                 metrics=metrics, splitter=splitter,
+                                 multioutput='raw_values')
+        gbt_results = gbt.validate(first=first, last=last,
+                                   metrics=metrics, splitter=splitter,
+                                   multioutput='raw_values')
 
         outpath.mkdir(parents=True, exist_ok=True)
         outpath_rf = outpath / f'rf_{shape_string}.csv'
         outpath_gbt = outpath / f'gbt_{shape_string}.csv'
 
-        rf_results.to_csv(str(outpath_rf), index_label='Target Hour')
-        gbt_results.to_csv(str(outpath_gbt), index_label='Target Hour')
+        results_df = pd.DataFrame(
+            index=target_hours, columns=[m.__name__ for m in metrics])
+        for metric in rf_results:
+            results_df[metric] = rf_results[metric]
+        results_df.to_csv(str(outpath_rf), index_label='Target Hour')
+
+        for metric in gbt_results:
+            results_df[metric] = gbt_results[metric]
+        results_df.to_csv(str(outpath_gbt), index_label='Target Hour')
+
+    print(f'Done.  Wrote results to {output_dir}')
